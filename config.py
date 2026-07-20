80ee8c2fcf5ca086ed2a6fb87ff1a44caad0fd5c
yuyou1342937357 <79406135+yuyou1342937357@users.noreply.github.com>
2026-07-20 17:36:58 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,46 @@
+# LUCAS：
+train_l8_folder_path = 'ReliSoilNet/dataset/l8_images/train'
+test_l8_folder_path = 'ReliSoilNet/dataset/l8_images/test'
+val_l8_folder_path = 'ReliSoilNet/dataset/l8_images/val'
+lucas_csv_path = 'ReliSoilNet/CSV/LUCAS_2015.csv'
+climate_csv_folder_path = "ReliSoilNet/dataset/Climate/LUCAS_Climate_Data"
+SIMCLR_PATH = "ReliSoilNet/results/RUN_LUCAS_HybridSSL_ViT_Trans_SelfSupervised_best.pth"
+
+
+
+
+# RaCA：
+# train_l8_folder_path = 'ReliSoilNet/dataset/l8_images_us/FT/train'
+# test_l8_folder_path = 'ReliSoilNet/dataset/l8_images_us/FT/test'
+# val_l8_folder_path = 'ReliSoilNet/dataset/l8_images_us/FT/val'
+# lucas_csv_path = 'ReliSoilNet/CSV/RaCA.csv'
+# climate_csv_folder_path = "ReliSoilNet/dataset/Climate/RaCA_Climate_Data"
+# SIMCLR_PATH = "ReliSoilNet/results/RUN_RaCA_HybridSSL_ViT_Trans_SelfSupervised_best.pth"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
